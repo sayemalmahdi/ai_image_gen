cd8c768d741bd23aa6ddd13cb223ff2dc3433439
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-02-03 21:09:27 +0600

    GenerateImageRequest() Function

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,44 @@ function onSubmit(e){
         alert('Please add some text');
         return;
     }
-    console.log(prompt, size);
+    generateImageRequest(prompt, size);
 }
+
+async function generateImageRequest(prompt, size) {
+    try {
+      showSpinner();
+  
+      const response = await fetch('/openai/generateimage', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt,
+          size,
+        }),
+      });
+  
+      if (!response.ok) {
+        removeSpinner();
+        throw new Error('That image could not be generated');
+      }
+  
+      const data = await response.json();
+       console.log(data);
+    
+       removeSpinner();
+    } catch (error) {
+      document.querySelector('.msg').textContent = error;
+    }
+  }
+  
+  function showSpinner() {
+    document.querySelector('.spinner').classList.add('show');
+  }
+  
+  function removeSpinner() {
+    document.querySelector('.spinner').classList.remove('show');
+  }
 
 document.querySelector('#image-form').addEventListener('submit', onSubmit);
