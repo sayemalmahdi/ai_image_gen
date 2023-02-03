2d9296b89e770ef63f4b68660a22db6cc5bd6155
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-02-03 14:08:09 +0600

    Route & Controller

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const port = process.env.PORT || 5200;
 
 const app = express();
 
+app.use('/openai', require('./routes/openaiRoutes'));
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
