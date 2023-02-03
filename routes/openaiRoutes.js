5006926e1a577b25e1aa69f43fc6125ae94146d1
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-02-03 18:39:58 +0600

    OpenAI Library Request/Response

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {generateImage} = require('../controllers/openaiController');
+const { generateImage } = require('../controllers/openaiController');
 const router = express.Router();
 
 router.post('/generateimage', generateImage);
