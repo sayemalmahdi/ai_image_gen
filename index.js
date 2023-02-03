0af5b7c5f8b6b90009922384e185e238c69667b6
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-02-03 13:49:35 +0600

    Express Server & ENV Variables

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log(123);
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5200;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
